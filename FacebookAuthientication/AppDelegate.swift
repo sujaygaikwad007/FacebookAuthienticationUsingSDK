2a5043a98f82f3147ed8e7879045c7e36f90244c
Aniket Patil <aniket.p@reapmind.com>
2023-12-20 18:42:21 +0530

    Final Commit

--- FacebookAuthientication/AppDelegate.swift
+++ FacebookAuthientication/AppDelegate.swift
@@ -1,20 +1,30 @@
-//
-//  AppDelegate.swift
-//  FacebookAuthientication
-//
-//  Created by Aniket Patil on 13/12/23.
-//
-
 import UIKit
+import FacebookCore
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
+    }
+
+    func application(
+            _ app: UIApplication,
+            open url: URL,
+            options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+        ) -> Bool {
+            ApplicationDelegate.shared.application(
+                app,
+                open: url,
+                sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+            )
+        }
+    
+    
     }
 
     // MARK: UISceneSession Lifecycle
@@ -31,6 +41,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
-}
 
--- FacebookAuthientication/ViewController.swift
+++ FacebookAuthientication/ViewController.swift
@@ -1,19 +1,28 @@
-//
-//  ViewController.swift
-//  FacebookAuthientication
-//
-//  Created by Aniket Patil on 13/12/23.
-//
 
 import UIKit
+import FacebookLogin
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let token = AccessToken.current,
+                !token.isExpired {
+            }
+        
+        let loginButton = FBLoginButton()
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+        
+        
+        loginButton.permissions = ["public_profile", "email"]
+          
+        
     }
-
-
+    
+    
 }
+
+
 
